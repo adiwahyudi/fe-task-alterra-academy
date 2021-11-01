6a308be62505a874717fcd32010cef9264c77dcf
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-11-02 01:12:20 +0700

    update Home

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomeBody from './HomeBody';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <HomeBody/>
+    </div>
+  );
+}
+
+export default App;
