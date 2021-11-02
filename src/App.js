79bc195116f4f74a97772cf26f298e071756c0b8
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-11-02 09:20:22 +0700

    update Contact Us

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import ContactUs from './ContactUs';
 import HomeBody from './HomeBody';
 import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <HomeBody/>
+      {/* <Navbar/>
+      <HomeBody/> */}
+      <ContactUs/>
     </div>
   );
 }
