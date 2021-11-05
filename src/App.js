09f08ae2393341b2f6d30aa2239f54a8e0479663
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-11-05 17:20:43 +0700

    exercise routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Home from './components/Home';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className='App'>
-
-      <Home/>
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/profile/:name" component={Profile}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
