c55c32043de5f22306fcaf88ef3c7382007d8900
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-11-09 11:12:11 +0700

    task form
    masih kurang

--- src/Pages/Form/FormPage.jsx
+++ src/Pages/Form/FormPage.jsx
@@ -1,131 +1,200 @@
-import React, {useState, useRef} from 'react'
+import React, { useRef, useState } from 'react'
 import style from './FormBuku.module.css'
 
 export default function FormPage() {
-    const dataKosong = {
-        judul: "",
-        pengarang: "",
-        cetakan: "",
-        tahunTerbit: "",
-        kotaTerbit: "",
-        harga: 0,
+
+  const baseData = {
+    nama: "",
+    email: "",
+    noHandphone: "",
+    pendidikan: "",
+    kelas: "",
+    harapan: "",
+  }
+
+  const baseError = {
+    nama: "",
+    email: "",
+    noHandphone: "",
+  }
+
+  const [data, setData] = useState(baseData)
+  const [errorMessage, setErrorMessage] = useState(baseError)
+  
+  const regexNama = /^[A-Za-z ]*$/
+  const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
+  const regexNoHP = /^(\d{9,14})$/
+
+  const suratKesungguhan = useRef('')
+
+
+  const handleInput = e => {
+    const name = e.target.name
+    const value = e.target.value
+
+    if (name === "nama") {
+      if (regexNama.test(value)) {
+        setErrorMessage({nama: ""})
+      } else {
+        setErrorMessage({nama: "Nama Lengkap Harus Berupa Huruf"})
+      }
     }
-    const [data,setData] = useState(dataKosong)
-    const regex = /^[A-Za-z ]*$/
-    const [errMsg, setErrMsg] = useState("")
-
-    const fotoSampul = useRef(null)
-
-    const handleInput = e => {
-        const name = e.target.name;
-        const value = e.target.value;
-
-        if (name === "pengarang"){
-            if (regex.test(value)){
-                setErrMsg("")
-            } else {
-                setErrMsg("Nama pengarang harus berupa huruf")
-            }
-        }
-        setData({
-            ...data,
-            [name]: value
-        })
-        // console.log("data",data);
+    
+    if (name === "email") {
+      if (regexEmail.test(value)) {
+        setErrorMessage({email: ""})
+      } else {
+        setErrorMessage({email: "Email Tidak Sesuai"})
+      }
     }
 
-    const handleSubmit = (event) => {
-        if (errMsg !== ""){
-            alert("Terdapat data kosong")
-        } else {
-            alert(`Data buku "${data.judul}" berhasil diterima`)
-        }
-        resetData()
-        event.preventDefault()
+    if (name === "noHandphone") {
+      if (regexNoHP.test(value)) {
+        setErrorMessage({noHandphone: ""})
+      } else {
+        setErrorMessage({noHandphone: "No Handphone Tidak Sesuai"})
+      }
     }
+    
+    console.log(errorMessage);
 
-    const resetData = () => {
-        setData(dataKosong)
-        setErrMsg("")
+    
+
+    setData({
+      ...data,
+      [name]: value
+    })
+
+  }
+
+  const handleSubmit = (event) => {
+    if (errorMessage !== "") {
+      alert("Data Pendaftar Tidak Sesuai")
+    } else {
+      alert(`Data Pendaftar "${data.nama}" Berhasil Diterima`)
+      resetData()
     }
-    return (
+    event.preventDefault()
+  }
+
+  const resetData = () => {
+    setData(baseData)
+    setErrorMessage("")
+  }
+
+  return (
+    <div>
+      <h1 style={{ "text-align": "center" }}>Pendaftaran Peserta Coding Bootcamp</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nama Lengkap:
+          <input 
+          type="text" 
+          name="nama" 
+          className={style.input} 
+          value={data.nama} 
+          onChange={handleInput} 
+          required />
+        </label>
+
+        <label>
+          Email:
+          <input 
+          type="text" 
+          name="email" 
+          className={style.input} 
+          value={data.email} 
+          onChange={handleInput} 
+          required />
+        </label>
+
+        <label>
+          No Handphone:
+          <input 
+          type="text" 
+          name="noHandphone" 
+          className={style.input} 
+          value={data.noHandphone} 
+          onChange={handleInput} 
+          required />
+        </label>
+        
+        <label style={{ "margin": "unset" }}>
+          Latar Belakang Pendidikan:
+          <div>
+            <input 
+            type="radio" 
+            name="pendidikan" 
+            value="IT" 
+            onChange={handleInput} 
+            checked={data.pendidikan === "IT" ? true : false} 
+            required />
+
+            <label>IT</label>
+
+            <input 
+            type="radio" 
+            name="pendidikan" 
+            value="Non IT"
+            onChange={handleInput} 
+            checked={data.pendidikan === "Non IT" ? true : false} 
+            required />
+
+            <label>Non IT</label>
+          </div>
+        </label>
+
+        <label>
+          Kelas Coding yang Dipilih:
+          <select 
+          type="text" 
+          name="kelas" 
+          className={style.input} 
+          value={data.kelas} 
+          onChange={handleInput} 
+          required>
+            <option value="" selected disabled hidden>Pilih Salah Satu Program</option>
+            <option>Coding BackEnd with Golang</option>
+            <option>Coding FrontEnd with ReactJS</option>
+            <option>Fullstack Developer</option>
+          </select>
+        </label>
+
+        <label>
+          Foto Surat Kesungguhan:
+          <input 
+          type="file" 
+          className={style.input} 
+          refs={suratKesungguhan} />
+        </label>
+
+        <label>
+          Harapan Untuk Coding Bootcamp ini:
+          <textarea 
+          name="harapan" 
+          id="" 
+          cols="30" 
+          rows="5" 
+          className={style.input} 
+          value={data.harapan} 
+          onChange={handleInput}></textarea>
+        </label>
+
+        {/* <p className={style.errorMessage}>{errorMessage.nama}</p>
+        <p className={style.errorMessage}>{errorMessage.email}</p>
+        <p className={style.errorMessage}>{errorMessage.noHandphone}</p> */}
+
+        <ul>
+            <li style={errorMessage.nama ? {color: 'red' } : { display : 'none'} }>{errorMessage.nama}</li>
+            <li style={errorMessage.email ? {color: 'red' } : { display : 'none'} }>{errorMessage.email}</li>
+            <li style={errorMessage.noHandphone ? {color: 'red' } : { display : 'none'} }>{errorMessage.noHandphone}</li>
+        </ul>
         <div>
-            <h1 style={{"text-align":"center"}}> Formulir Buku Baru</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Judul:
-                    <input 
-                        type="text"
-                        name="judul"
-                        onChange={handleInput}
-                        required
-                        value={data.judul}
-                        className={style.input}
-                    />
-                </label>
-                <label>
-                    Pengarang:  
-                    <input 
-                        type="text"
-                        name="pengarang"
-                        onChange={handleInput}
-                        required
-                        value={data.pengarang}
-                        className={style.input}
-                    />
-                </label>
-                <label>
-                    Cetakan:
-                    <input 
-                        type="text"
-                        name="cetakan"
-                        onChange={handleInput}
-                        value={data.cetakan}
-                        className={style.input}
-                    />
-                </label>
-                <label>
-                    Tahun Terbit:
-                    <input 
-                        type="number"
-                        name="tahunTerbit"
-                        onChange={handleInput}
-                        value={data.tahunTerbit}
-                        className={style.input}
-                    />
-                </label>
-                <label>
-                    Kota Terbit:
-                    <input 
-                        type="text"
-                        name="kotaTerbit"
-                        onChange={handleInput}
-                        value={data.kotaTerbit}
-                        className={style.input}
-                    />
-                </label>
-                <label>
-                    Harga:
-                    <input 
-                        type="number"
-                        name="harga"
-                        onChange={handleInput}
-                        value={data.harga}
-                        className={style.input}
-                    />
-                </label>
-                <label>
-                    Foto Samput:
-                    <input 
-                        type="file"
-                        refs={fotoSampul}
-                        className={style.input}
-                    />
-                </label>
-                <span className={style.errorMessage}>{errMsg}</span>
-                <input className={style.btn} type="submit" value="submit" />
-                <button className={style.btn} onClick={resetData} >Reset</button>
-            </form>
+          <input type="submit" className={style.btn} value="Submit" />
+          <button className={`${style.btn} ${style.btnReset}`} onClick={resetData}>Reset</button>
         </div>
-    )
-}   
+
+      </form>
+    </div>
+  )
+}
